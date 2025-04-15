76078a7708ef62d0345ccbe6ef6aab30f861cf57
FakePsycho <endtheblack71@gmail.com>
2025-04-15 13:51:10 +0500

    Centerda Salom

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,76 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: Sad(),));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Sad extends StatefulWidget {
+  const Sad({super.key});
 
+  @override
+  State<Sad> createState() => _SadState();
+}
 
+class _SadState extends State<Sad> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'sss',
-      theme: ThemeData(
-      
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'salom hammaga'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
+    return Scaffold(body: Center(child: Text("Salom")));
   }
 }
